282c0e957d78449bdcd2df1467479e2dbe90ae23
Elijah Wilsher <elijahwilsher@gmail.com>
2024-07-16 08:30:30 +0000

    added query chain

--- myApp.js
+++ myApp.js
@@ -130,8 +130,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favouriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({name: 1, age: 0, favouriteFoods: 1}).exec((err, people) => {
+    if(err) console.log(err)
+      done(null, people)
+  })
 };
 
 /** **Well Done !!**
